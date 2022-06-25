e202c3df57d0266ec114630822f19a52949c75c1
lsantana <lsantana@student.42sp.org.br>
2022-06-25 05:34:29 +0200

    StrDup

---
+++ ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsantana <lsantana@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/11 15:04:12 by lsantana          #+#    #+#             */
+/*   Updated: 2022/06/17 01:49:04 by lsantana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strdup(const char *str)
+{
+	char	*new_str;
+	int		i;
+
+	i = 0;
+	new_str = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1));
+	if (!new_str)
+		return (NULL);
+	while (str[i])
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
