86e711aad26813f2ae0e95a62df1682dbda791d6
lsantana <lsantana@student.42sp.org.br>
2022-06-25 05:37:25 +0200

    StrrChr

---
+++ ft_strrchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsantana <lsantana@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/03 21:47:57 by lsantana          #+#    #+#             */
+/*   Updated: 2022/06/21 16:44:46 by lsantana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+#include "libft.h"
+
+char	*ft_strrchr(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	while (i >= 0)
+	{
+		if (str[i] == (char)c)
+			return ((char *)(str + i));
+		i--;
+	}
+	return (NULL);
+}
